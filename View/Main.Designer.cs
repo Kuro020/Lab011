adec85c33779c6b62f4b6aa8e194ca6718243bb3
MATEUS FORTUNATO XAVIER <mateus.fxavier@senacsp.edu.br>
2022-05-13 17:29:15 -0300

    Adicionar arquivos de projeto.

---
+++ View/Main.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Lab011.View
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxEndereco = new System.Windows.Forms.TextBox();
+            this.tbxDe = new System.Windows.Forms.TextBox();
+            this.tbxPara = new System.Windows.Forms.TextBox();
+            this.btnTrocar = new System.Windows.Forms.Button();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Endereço:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "De:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Para:";
+            // 
+            // tbxEndereco
+            // 
+            this.tbxEndereco.Location = new System.Drawing.Point(12, 38);
+            this.tbxEndereco.Name = "tbxEndereco";
+            this.tbxEndereco.Size = new System.Drawing.Size(286, 23);
+            this.tbxEndereco.TabIndex = 3;
+            // 
+            // tbxDe
+            // 
+            this.tbxDe.Location = new System.Drawing.Point(12, 98);
+            this.tbxDe.Name = "tbxDe";
+            this.tbxDe.Size = new System.Drawing.Size(223, 23);
+            this.tbxDe.TabIndex = 4;
+            // 
+            // tbxPara
+            // 
+            this.tbxPara.Location = new System.Drawing.Point(12, 158);
+            this.tbxPara.Name = "tbxPara";
+            this.tbxPara.Size = new System.Drawing.Size(223, 23);
+            this.tbxPara.TabIndex = 5;
+            // 
+            // btnTrocar
+            // 
+            this.btnTrocar.Location = new System.Drawing.Point(263, 129);
+            this.btnTrocar.Name = "btnTrocar";
+            this.btnTrocar.Size = new System.Drawing.Size(114, 52);
+            this.btnTrocar.TabIndex = 6;
+            this.btnTrocar.Text = "Trocar";
+            this.btnTrocar.UseVisualStyleBackColor = true;
+            this.btnTrocar.Click += new System.EventHandler(this.btnTrocar_Click);
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(304, 38);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(38, 24);
+            this.btnPesquisar.TabIndex = 7;
+            this.btnPesquisar.Text = "...";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(410, 234);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.btnTrocar);
+            this.Controls.Add(this.tbxPara);
+            this.Controls.Add(this.tbxDe);
+            this.Controls.Add(this.tbxEndereco);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.Text = "Troca nome arquivo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxEndereco;
+        private System.Windows.Forms.TextBox tbxDe;
+        private System.Windows.Forms.TextBox tbxPara;
+        private System.Windows.Forms.Button btnTrocar;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
---
+++ View/Main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab011.View
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            tbxEndereco.Text = string.Empty;
+
+            try
+            {
+                if(folderBrowserDialog1.ShowDialog() != DialogResult.Cancel)
+                {
+                    tbxEndereco.Text = folderBrowserDialog1.SelectedPath;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Alerta de Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnTrocar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (tbxEndereco.Text.Trim() == string.Empty)
+                {
+                    throw new Exception("Selecione uma pasta para efetivar a troca de arquivo.");     
+                }
+
+                if (tbxDe.Text.Trim() == string.Empty)
+                {
+                    throw new Exception("Informe o valor procurado para a troca.");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Alerta de Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
